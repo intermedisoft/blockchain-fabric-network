48c951300fc1f5490610f200c5f02b6600da78b2
Seksit Disaro <zekzit@gmail.com>
2018-02-08 15:17:16 +0700

    map volumes with host

--- scripts/02_run_genscript.sh
+++ scripts/02_run_genscript.sh
@@ -1,4 +1,4 @@
-#/bin/bash
-./bin/cryptogen generate --config=./crypto-config.yaml
+#!/bin/bash
 export FABRIC_CFG_PATH=$PWD
+./bin/cryptogen generate --config=./crypto-config.yaml
 ./bin/configtxgen -profile DepaOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
---
+++ scripts/03_create_volumes_folder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p /var/hyperledger/ca0
+mkdir -p /var/hyperledger/ca1
+mkdir -p /var/hyperledger/ca2
+mkdir -p /var/hyperledger/couchdb0
+mkdir -p /var/hyperledger/couchdb1
+mkdir -p /var/hyperledger/couchdb2
+mkdir -p /var/hyperledger/mongo0
+mkdir -p /var/hyperledger/mongo1
+mkdir -p /var/hyperledger/mongo2
+mkdir -p /var/hyperledger/peer0.phr
+mkdir -p /var/hyperledger/peer0.pensook
+mkdir -p /var/hyperledger/peer0.bpk
