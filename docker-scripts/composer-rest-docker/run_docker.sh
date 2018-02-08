1edf25bec0975747b979c8b6ca0279eabc90325d
Seksit Disaro <zekzit@gmail.com>
2018-02-08 16:05:10 +0700

    change script's file permission, add docker script of composer rest docker

---
+++ docker-scripts/composer-rest-docker/run_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source envvars.txt
+docker run \
+    -d \
+    -e COMPOSER_CARD=${COMPOSER_CARD} \
+    -e COMPOSER_NAMESPACES=${COMPOSER_NAMESPACES} \
+    -e COMPOSER_AUTHENTICATION=${COMPOSER_AUTHENTICATION} \
+    -e COMPOSER_MULTIUSER=${COMPOSER_MULTIUSER} \
+    -e COMPOSER_PROVIDERS="${COMPOSER_PROVIDERS}" \
+    -e COMPOSER_DATASOURCES="${COMPOSER_DATASOURCES}" \
+    -v ~/.composer:/home/composer/.composer \
+    --name rest \
+    --network composer_default \
+    -p 3000:3000 \
+    depa/blockchain-composer-rest-server
