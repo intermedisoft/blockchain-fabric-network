e0078d07a35695dcd76efe6361cd948fd37c90e9
Seksit Disaro <zekzit@imed-mm.local>
2018-02-14 16:33:16 +0700

    fix channel name

--- scripts/02_run_genscript.sh
+++ scripts/02_run_genscript.sh
@@ -3,8 +3,8 @@ export FABRIC_CFG_PATH=$PWD
 
 ./bin/cryptogen generate --config=./crypto-config.yaml
 ./bin/configtxgen -profile DepaOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-./bin/configtxgen -profile DepaChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID DepaChannel
-./bin/configtxgen -profile DepaChannel -outputAnchorPeersUpdate ./channel-artifacts/PHRBloxMSPanchors.tx -channelID DepaChannel -asOrg PHRBloxMSP
-./bin/configtxgen -profile DepaChannel -outputAnchorPeersUpdate ./channel-artifacts/BPKMSPanchors.tx -channelID DepaChannel -asOrg BPKMSP
-./bin/configtxgen -profile DepaChannel -outputAnchorPeersUpdate ./channel-artifacts/PensookMSPanchors.tx -channelID DepaChannel -asOrg PensookMSP
+./bin/configtxgen -profile depa_channel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID depa_channel
+./bin/configtxgen -profile depa_channel -outputAnchorPeersUpdate ./channel-artifacts/PHRBloxMSPanchors.tx -channelID depa_channel -asOrg PHRBloxMSP
+./bin/configtxgen -profile depa_channel -outputAnchorPeersUpdate ./channel-artifacts/BPKMSPanchors.tx -channelID depa_channel -asOrg BPKMSP
+./bin/configtxgen -profile depa_channel -outputAnchorPeersUpdate ./channel-artifacts/PensookMSPanchors.tx -channelID depa_channel -asOrg PensookMSP
 
