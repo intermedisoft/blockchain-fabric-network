070c7f4fd78d455adfb09aaffda31792880780f4
Seksit Disaro <zekzit@imed-mm.local>
2018-02-15 17:16:32 +0700

    fix bug

--- scripts/04_join_channel.sh
+++ scripts/04_join_channel.sh
@@ -4,7 +4,7 @@ export ORDERER_CA=/var/hyperledger/crypto-config/ordererOrganizations/ehrblox.co
 echo ==== for PHR node
 echo docker exec -it peer0.phr.ehrblox.com peer channel create -o orderer.ehrblox.com:7050 -c depachannel -f /var/hyperledger/channel-artifacts/channel.tx --tls --cafile /var/hyperledger/crypto-config/ordererOrganizations/ehrblox.com/orderers/orderer.ehrblox.com/msp/tlscacerts/tlsca.ehrblox.com-cert.pem --logging-level debug
 echo docker exec -it peer0.phr.ehrblox.com peer channel update -o orderer.ehrblox.com:7050 -c depachannel -f /var/hyperledger/channel-artifacts/PHRBloxMSPanchors.tx --tls true --cafile $ORDERER_CA 
-echo docker exec -it peer0.phr.ehrblox.com mv /opt/gopath/src/github.com/hyperledger/fabric/peer /var/hyperledger/channel-artifacts-write
+echo docker exec -it peer0.phr.ehrblox.com mv /opt/gopath/src/github.com/hyperledger/fabric/peer/depachannel.block /var/hyperledger/channel-artifacts-write
 echo docker exec -it peer0.phr.ehrblox.com peer channel join -b /var/hyperledger/channel-artifacts-write/depachannel.block
 
 echo ==== for Pensook node
