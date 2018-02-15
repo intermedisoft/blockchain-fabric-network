7fa0343a631f234bd884daaa3a8282af7be1a4f5
Seksit Disaro <zekzit@imed-mm.local>
2018-02-15 17:00:58 +0700

    add script for support channel creation, join

--- scripts/04_join_channel.sh
+++ scripts/04_join_channel.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 export ORDERER_CA=/var/hyperledger/crypto-config/ordererOrganizations/ehrblox.com/orderers/orderer.ehrblox.com/msp/tlscacerts/tlsca.ehrblox.com-cert.pem
-echo peer channel create -o orderer.ehrblox.com:7050 -c depa_channel -f /var/hyperledger/channel-artifacts/channel.tx --tls true --cafile $ORDERER_CA
-echo peer channel update -o orderer.ehrblox.com:7050 -c depa_channel -f /var/hyperledger/channel-artifacts/PHRBloxMSPanchors.tx --tls true --cafile $ORDERER_CA 
-echo peer channel update -o orderer.ehrblox.com:7050 -c depa_channel -f /var/hyperledger/channel-artifacts/BPKMSPanchors.tx --tls true --cafile $ORDERER_CA 
-echo peer channel update -o orderer.ehrblox.com:7050 -c depa_channel -f /var/hyperledger/channel-artifacts/PensookMSPanchors.tx --tls true --cafile $ORDERER_CA 
 
-echo peer channel join -b depa_channel.block 
+echo ==== for PHR node
+echo docker exec -it peer0.phr.ehrblox.com peer channel create -o orderer.ehrblox.com:7050 -c depachannel -f /var/hyperledger/channel-artifacts/channel.tx --tls --cafile /var/hyperledger/crypto-config/ordererOrganizations/ehrblox.com/orderers/orderer.ehrblox.com/msp/tlscacerts/tlsca.ehrblox.com-cert.pem --logging-level debug
+echo docker exec -it peer0.phr.ehrblox.com peer channel update -o orderer.ehrblox.com:7050 -c depachannel -f /var/hyperledger/channel-artifacts/PHRBloxMSPanchors.tx --tls true --cafile $ORDERER_CA 
+echo docker exec -it peer0.phr.ehrblox.com mv /opt/gopath/src/github.com/hyperledger/fabric/peer /var/hyperledger/channel-artifacts-write
+echo docker exec -it peer0.phr.ehrblox.com peer channel join -b /var/hyperledger/channel-artifacts-write/depachannel.block
+
+echo ==== for Pensook node
+echo docker exec -it peer0.pensook.ehrblox.com peer channel update -o orderer.ehrblox.com:7050 -c depachannel -f /var/hyperledger/channel-artifacts/PensookMSPanchors.tx --tls true --cafile $ORDERER_CA 
+echo docker exec -it peer0.pensook.ehrblox.com peer channel join -b /var/hyperledger/channel-artifacts/depachannel.block
+
+echo ==== for BPK node
+echo docker exec -it peer0.bpk.ehrblox.com peer channel update -o orderer.ehrblox.com:7050 -c depachannel -f /var/hyperledger/channel-artifacts/BPKMSPanchors.tx --tls true --cafile $ORDERER_CA 
+echo docker exec -it peer0.bpk.ehrblox.com peer channel join -b /var/hyperledger/channel-artifacts/depachannel.block
+echo
